6bad434b8217d0231887f35dc601ffeec472057d
Christby <btian@ucsd.edu>
2022-02-04 14:28:27 -0800

    JUnit added and test file created

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void minusTest() {
+        assertEquals(5, SkillDemo.minus(15,5));
+    }
+}
